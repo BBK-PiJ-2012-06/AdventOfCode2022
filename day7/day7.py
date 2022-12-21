38fa220e294d0a4d1f7d10bf0edd5b24bf5ae7ec
Simon Ward <simon.ward@barnett-waddingham.co.uk>
2022-12-21 19:39:00 +0000

    Clean up

--- day7/day7.py
+++ day7/day7.py
@@ -18,33 +18,33 @@ for command in commandLog[1:]:
         continue
 
     if re.search(dir, command):
-        dirName = re.split(dir, command)[1]
-        currentDirectory.append(Directory(name=dirName, parent=currentDirectory))
+        name = re.split(dir, command)[1]
+        currentDirectory.append(Directory(name, parent=currentDirectory))
         continue
 
     if re.search(file, command):
         size, name = command.split(' ')
-        currentDirectory.append(File(name=name, size=int(size)))
+        currentDirectory.append(File(name, int(size)))
         continue
 
     if re.search(cd, command):
-        dirName = re.split(cd, command)[1]
-        if dirName == '..':
+        name = re.split(cd, command)[1]
+        if name == '..':
             currentDirectory = currentDirectory.parent
         else:
-            currentDirectory = next(dir for dir in currentDirectory.contents if isinstance(dir, Directory) and dir.name == dirName)
+            currentDirectory = next(dir for dir in currentDirectory.contents if isinstance(dir, Directory) and dir.name == name)
 
 # part 1: find the sum of sizes of all directories of size <= 100000
-def part1Traverse(dir: Directory) -> int:
+def part1Sum(dir: Directory) -> int:
     sum = 0
     for d in filter(lambda x: isinstance(x, Directory), dir.contents):
         size = d.getSize()
         if size <= 100000:
             sum += size
-        sum += part1Traverse(d)
+        sum += part1Sum(d)
     return sum
 
-print('Part 1:', part1Traverse(root))
+print('Part 1:', part1Sum(root))
 
 # part 2: the total disk space is 70,000,000 and we need 30,000,000 free space. 
 # we need to find the size of the smallest directory that, if deleted, would free up enough space.
@@ -54,16 +54,16 @@ spaceUsed = root.getSize()
 spaceAvailable = totalSpace - spaceUsed
 spaceToFreeUp = spaceNeeded - spaceAvailable
 
-def part2Traverse(dir: Directory):
+def deletionCandidateSizes(dir: Directory):
     candidates = []
     for d in filter(lambda x: isinstance(x, Directory), dir.contents):
         size = d.getSize()
         if size >= spaceToFreeUp:
             candidates.append(size)
-        candidates.extend(part2Traverse(d))
+        candidates.extend(deletionCandidateSizes(d))
     return candidates
 
-candidateDirSizes = part2Traverse(root)
-print('Part 2:', min(candidateDirSizes))
+candidates = deletionCandidateSizes(root)
+print('Part 2:', min(candidates))
 
 input.close()
